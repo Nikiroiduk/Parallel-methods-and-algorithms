52a852f8158624f745c468d6439f6beab6ae605e
Nikita Isyuk <Nikita.Isyuk@outlook.com>
2024-09-12 17:49:04 +0300

    Add project files.

---
+++ Ex1/DownloadUrls.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal static class DownloadUrls
+    {
+        public static async Task<List<string>> Run()
+        {
+            List<string> urlsData = new();
+
+
+
+            return [];
+        }
+    }
+}
---
+++ Ex1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        string projectDirectory = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\.."));
+        string filePath = Path.Combine(projectDirectory, "Ex1List.txt");
+
+        List<string> urls = new();
+
+        using (var reader = new StreamReader(filePath))
+        {
+            try
+            {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        urls.Add(line);
+                    }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading file: {ex.Message}");
+            }
+        }
+
+        using (var client = new HttpClient())
+        {
+            try
+            {
+                foreach (var url in urls)
+                {
+                    string data = await client.GetStringAsync(url);
+                    Console.WriteLine(data);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Error downloading data: {e.Message}");
+            }
+        }
+    }
+}
